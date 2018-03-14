9828231249327fea73dc834701d92742d34db9b9
Mubarak01 <mubaracksulaymann@gmail.com>
2018-03-14 09:37:47 +0100

    A Review

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// var colors = ['#009acd', '#008b45', '#ffd700', '#cd2626', ''];
+// var active = 0;
+// setInterval(function()
+// {
+//     document.querySelector('.identity').style.background = colors[active];
+//     active++;
+//     if (active == colors.length) active = 0;
+// }, 1000);
